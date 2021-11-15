75a765221e6c12f35524b291d30d0b185674a4ec
Valon <Saidi>
2021-11-15 01:22:49 +0100

    deleted unused icons

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -2867,7 +2867,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3327" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11269" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
